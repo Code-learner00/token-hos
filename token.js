8ebdd7102a818cc4cdea0cb0d6b81b0e7a79f873
BHAVYA SAI SRI <124481659+Code-learner00@users.noreply.github.com>
2024-07-03 07:28:07 +0530

    Update token.js

--- token.js
+++ token.js
@@ -1,49 +1,32 @@
-const util = window.browserUtilities.util;
+// token.js
 function printReceipt() {
-    // Get the values from the input fields
-    const hospitalName = document.getElementById('hospital-name').value;
-    const patientName = document.getElementById('patient-name').value;
-    const visitorName = document.getElementById('visitor-name').value;
-    const visitorPhNo = document.getElementById('visitor-ph-no').value;
-    const tokenNo = document.getElementById('token-no').value;
-    const dateTime = document.getElementById('date-time').value;
-    const time = document.getElementById('time').value;
-  
-    // Create a receipt string
-    let receipt = `
-      ${hospitalName}\n
-      Patient Name: ${patientName}\n
-      Visitor Name: ${visitorName}\n
-      Visitor Phone No: ${visitorPhNo}\n
-      Token No: ${tokenNo}\n
-      Date and Time: ${dateTime}\n
-      Time: ${time}\n
-    `;
+  // Get the values from the input fields
+  const hospitalName = document.getElementById('hospital-name').value;
+  const patientName = document.getElementById('patient-name').value;
+  const visitorName = document.getElementById('visitor-name').value;
+  const visitorPhNo = document.getElementById('visitor-ph-no').value;
+  const tokenNo = document.getElementById('token-no').value;
+  const dateTime = document.getElementById('date-time').value;
+  const time = document.getElementById('time').value;
 
+  // Create a receipt string
+  let receipt = `
+    ${hospitalName}\n
+    Patient Name: ${patientName}\n
+    Visitor Name: ${visitorName}\n
+    Visitor Phone No: ${visitorPhNo}\n
+    Token No: ${tokenNo}\n
+    Date and Time: ${dateTime}\n
+    Time: ${time}\n
+  `;
 
-    
-    // Print the receipt using 
-    printJS({
-      printable: receipt,
-      type: 'raw',
-      font: 'onospace',
-      fontSize: 12,
-      align: 'center',
-      margins: { top: 10, bottom: 10, left: 10, right: 10 },
-    });
-  }
-
-
-  const printer = new escpos.Printer({
-    device: '/dev/usb/printer', // or 'Bluetooth:printer'
-    encoding: 'UTF8',
-    timeout: 5000,
+  // Print the receipt using printJS
+  printJS({
+    printable: receipt,
+    type: 'raw',
+    font: 'onospace',
+    fontSize: 12,
+    align: 'center',
+    margins: { top: 10, bottom: 10, left: 10, right: 10 },
   });
-  
-  printer
-    .font('a')
-    .align('ct')
-    .style('bu')
-    .size(1, 1)
-  
-    .cut();
+}
